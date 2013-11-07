a6aa9839ace03a55d4a6641c5475d7e93b3ff15b
benzonico <nicolas.peru@gmail.com>
2013-11-07 23:37:05 +0100

    added frequencies route to return frequencies of calls

--- src/main/java/com/bzn/codestory/elevator/ElevatorEngine.java
+++ src/main/java/com/bzn/codestory/elevator/ElevatorEngine.java
@@ -9,6 +9,19 @@ import spark.Route;
 public class ElevatorEngine {
 
 	private final static Elevator elevator = new Elevator();
+	private static int[] frequencies;
+
+	private static void incrementFrequency(int floor) {
+		int[] freq = getFrequencies();
+		freq[floor] = freq[floor] + 1;
+	}
+
+	private static int[] getFrequencies() {
+		if (frequencies == null) {
+			frequencies = new int[100];
+		}
+		return frequencies;
+	}
 
 	public static void main(String[] args) {
 		ElevatorEngine.initServices(Integer.parseInt(System
@@ -23,6 +36,7 @@ public class ElevatorEngine {
 				int floor = Integer.valueOf(req.queryParams("atFloor"));
 				Direction direction = Direction.valueOf(req.queryParams("to"));
 				elevator.call(floor, direction);
+				incrementFrequency(floor);
 				resp.status(200);
 				return resp;
 			}
@@ -54,6 +68,13 @@ public class ElevatorEngine {
 		get(new Route("reset") {
 			@Override
 			public Object handle(Request req, Response resp) {
+				int lower = Integer.valueOf(req.queryParams("lowerFloor"));
+				int higher = Integer.valueOf(req.queryParams("higherFloor"));
+
+				System.out.println("RESET : cause : "
+						+ req.queryParams("cause"));
+
+				frequencies = new int[higher - lower + 1];
 				elevator.reset();
 				resp.status(200);
 				return resp;
@@ -66,6 +87,21 @@ public class ElevatorEngine {
 				resp.status(200);
 				return elevator.nextCommand();
 			}
+		});
+
+		get(new Route("frequencies") {
+
+			@Override
+			public Object handle(Request req, Response resp) {
+				String result = "<table>";
+				for (int i = getFrequencies().length - 1; i >= 0; i--) {
+					result += "<tr><td>" + i + "</td><td>" + frequencies[i]
+							+ "</td></tr>";
+				}
+				result += "</table>";
+				return result;
+			}
+
 		});
 	}
 
--- src/test/java/com/bzn/codestory/elevator/ElevatorEngineTest.java
+++ src/test/java/com/bzn/codestory/elevator/ElevatorEngineTest.java
@@ -14,22 +14,29 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ElevatorEngineTest {
 
 	private static final HttpClient httpclient = new DefaultHttpClient();
+	private static int PORT = 8000;
+
+	@BeforeClass
+	public static void setup() {
+		ElevatorEngine.initServices(PORT);
+	}
 
 	@Test
 	public void initServices_should_listen_on_specified_port()
 			throws ClientProtocolException, IOException {
-		int port = 8000;
-		ElevatorEngine.initServices(port);
 		String[] domains = { "go", "call", "reset", "userHasEntered",
 				"userHasExited", "nextCommand" };
+		String[] params = { "?floorToGo=1", "?atFloor=2&to=UP",
+				"?lowerFloor=0&higherFloor=12&cause=toujours", "", "", "" };
 		for (int i = 0; i < domains.length; i++) {
-			HttpGet get = new HttpGet("http://localhost:" + port + "/"
-					+ domains[i]);
+			HttpGet get = new HttpGet("http://localhost:" + PORT + "/"
+					+ domains[i] + params[i]);
 			HttpResponse response = httpclient.execute(get);
 			// required to release the connection
 			getResponseAsString(get, response.getEntity());
@@ -46,6 +53,35 @@ public class ElevatorEngineTest {
 		String result = reader.readLine();
 		instream.close();
 		return result;
+	}
+
+	@Test
+	public void frequencies_should_be_displayed_after_reset_and_calls()
+			throws ClientProtocolException, IOException {
+		String[] domains = { "reset", "call", "call" };
+		String[] params = { "?lowerFloor=0&higherFloor=2&cause=toujours",
+				"?atFloor=1&to=UP", "?atFloor=1&to=UP" };
+		for (int i = 0; i < domains.length; i++) {
+			HttpGet get = new HttpGet("http://localhost:" + PORT + "/"
+					+ domains[i] + params[i]);
+			HttpResponse response = httpclient.execute(get);
+			// required to release the connection
+			getResponseAsString(get, response.getEntity());
+			int code = response.getStatusLine().getStatusCode();
+			assertThat(code).isEqualTo(HttpStatus.SC_OK);
+		}
+
+		HttpGet get = new HttpGet("http://localhost:" + PORT + "/frequencies");
+		HttpResponse response = httpclient.execute(get);
+		// required to release the connection
+		String freqHTML = getResponseAsString(get, response.getEntity());
+		int code = response.getStatusLine().getStatusCode();
+		assertThat(code).isEqualTo(HttpStatus.SC_OK);
+		assertThat(freqHTML).isEqualTo(
+				"<table>" + "<tr><td>2</td><td>0</td></tr>"
+						+ "<tr><td>1</td><td>2</td></tr>"
+						+ "<tr><td>0</td><td>0</td></tr></table>");
+
 	}
 
 }
